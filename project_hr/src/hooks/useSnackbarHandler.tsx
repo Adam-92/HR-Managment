57cacd46a7af4b4358a86c2307899ddd822e4b05
Adam-92 <adam.kalcas2@gmail.com>
2023-08-03 12:12:29 +0200

    creating MR

--- project_hr/src/hooks/useSnackbarHandler.tsx
+++ project_hr/src/hooks/useSnackbarHandler.tsx
@@ -1,7 +1,6 @@
-import { VariantType, useSnackbar } from 'notistack';
+import { type VariantType, useSnackbar } from 'notistack';
 
 export const useSnackbarHandler = () => {
-  /* Penwie się przyda, a nie ma sensu tworzyć wszędzie funkcji handleClickVariant */
   const { enqueueSnackbar } = useSnackbar();
 
   const handleClickVariant = (message: string, variant: VariantType) => {
